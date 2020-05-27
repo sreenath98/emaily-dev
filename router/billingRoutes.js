abd09e742bc54260469935d55c7e36e8367ee0a0
sreenath98 <sree98296@gmail.com>
2020-05-27 17:35:21 +0530

    git added

--- router/billingRoutes.js
+++ router/billingRoutes.js
@@ -1,14 +1,19 @@
 const keys = require ('../config/keys');
 const Stripe = require('stripe')(keys.stripeSecretKey);
+const requireLogin = require('../middleware/requirelogin');
 
 module.exports = (app) => {
-  app.post("/api/stripe", async (req, res) => {
-   const charge = await Stripe.charges.create({
+  app.post("/api/stripe",requireLogin, async (req, res) => {
+  const charge = await Stripe.charges.create({
       amount:500,
       currency:'usd',
       description:'$5 for 5 credits',
       source:req.body.id,
     });
-    Console.log(charge);
+    
+    req.user.credits += 5;
+    const user = await req.user.save();
+
+    res.send(user);
   });
 };
